7de6b2de099a9f95139fa530cb9e774d1ac1ca55
Sree-Lakshmi-Sai <sreelakshmisai15@gmail.com>
2025-06-04 09:29:21 +0530

    Add files via upload

---
+++ n_to_one.py
@@ -0,0 +1,14 @@
+def num(n):
+    for i in range(1,n+1):
+        print(n)
+        n -= 1
+n=int(input())
+num(n)
+
+def num(n):
+    i=1
+    while n>=i:
+        print(n)
+        n-=1
+n=int(input())
+num(n)
