fea2535a45d1147c356d4f4396feb2cb466587e8
Sree-Lakshmi-Sai <sreelakshmisai15@gmail.com>
2025-06-04 09:37:26 +0530

    Add files via upload

---
+++ pattern10.py
@@ -0,0 +1,18 @@
+# * * * *
+# *     *
+# *     *
+# * * * *
+
+n =10
+for i in range(1,n+1):
+    if i == 1 or i == n: print("* "*n)
+    else: print("* "+"  "*(n-2)+"* ")
+
+#============  2 for
+n = 9
+for i in  range(1,n+1):
+    for j in range(1,n+1):
+        if i==1 or j == 1 or i == n or j == n:
+            print("* ",end="")
+        else: print("  ",end="")
+    print()
